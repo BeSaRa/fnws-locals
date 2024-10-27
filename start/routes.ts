3f5981f1e71b70e25bd911dc2caf535ea9572cbc
BeSaRa <ahmedbesara@gmailcom>
2024-10-27 13:22:36 +0300

    init commit

---
+++ start/routes.ts
@@ -0,0 +1,35 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import router from '@adonisjs/core/services/router'
+import * as fs from 'node:fs'
+
+const LOCALS_PATH = '../resources/locals.json'
+
+router.get('/', async () => {
+  const locals = fs.readFileSync(LOCALS_PATH, 'utf8')
+  return locals
+})
+
+router.get('/key/:key', async (ctx) => {
+  const locals = getLocals()
+  return locals[ctx.params.key]
+})
+
+router.post('/', async (ctx: HttpContext) => {
+  const value = ctx.request.body() as unknown as { key: string, ar: string, en: string }
+  const content = getLocals()
+  content[value.key] = {
+    ar: value.ar,
+    en: value.en
+  }
+  saveLocals(content)
+  return content[value.key]
+})
+
+
+function getLocals() {
+  return JSON.parse(fs.readFileSync(LOCALS_PATH, 'utf8'))
+}
+
+function saveLocals(content: any) {
+  return fs.writeFileSync(LOCALS_PATH, JSON.stringify(content, null, ' '), 'utf8')
+}
